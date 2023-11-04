f5aa11b8cd9eae10ccc3d95cc14ded82b64613d5
leake <carl.leake@jpl.nasa.gov>
2023-11-03 20:14:41 -0700

    Adding shebang for the users shell. See #4.

--- src/main.rs
+++ src/main.rs
@@ -135,7 +135,8 @@ impl State {
         self.stop_run();
         create_file_with_text(
             &self.multitask_file,
-            &format!("{}\n{}\n{}\n{}\n",
+            &format!("{}{}\n\n{}\n{}\n{}\n{}\n",
+                "#!", self.shell,
                 "# Hi there! Anything below these lines will be executed on save.",
                 "# One command per line.",
                 "# Place empty lines between steps that should run in parallel.",
